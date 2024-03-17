0e2f60d62d1ebfe1cc79a9beed3e6a604e0de999
marceloyuba <marceloyuba@gmail.com>
2024-03-17 18:25:35 -0300

    commit inicial

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ with st.container():
     st.subheader("Hola, soy Marcelo Yuba :wave:")
     st.title("Un Data Analyst de Buenos Aires, Argentina")
     st.write(
-        "Soy apacionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para negocios."
+        "Soy apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para negocios."
     )
     st.write("[Mas sobre mi >](https://github.com/marceloyuba)")
