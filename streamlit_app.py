0a84e3becac2fbdc2706d1a514ba4b542302270c
marceloyuba <marceloyuba@gmail.com>
2024-03-17 19:49:46 -0300

    commit powerbi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from IPython.display import IFrame
+
 st.set_page_config(page_title="Prueba", page_icon=":tada:", layout="wide")
-dashboard = IFrame(src="https://app.powerbi.com/view?r=eyJrIjoiM2NjNDA0YmItMmRhZC00ZDhlLWFmOWYtZTZiMWMxYWY3ODAzIiwidCI6ImUyYjc5Nzc5LTBhODgtNDMzMS05YjQyLTM4NGNkNzFjODVkNyIsImMiOjR9", width=1000, height=600)
+
 # ---- HEADER SECTION ----
 with st.container():
     st.subheader("Hola, soy Marcelo Yuba :wave:")
@@ -10,4 +10,11 @@ with st.container():
         "Soy apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para negocios."
     )
     st.write("[Mas sobre mi >](https://github.com/marceloyuba)")
-with st.container(): dashboard
+def main():
+    st.title('Dashboard de Power BI en Streamlit')
+    
+    # Inserta el iframe con la URL de tu dashboard de Power BI
+    st.markdown('<iframe width="800" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiM2NjNDA0YmItMmRhZC00ZDhlLWFmOWYtZTZiMWMxYWY3ODAzIiwidCI6ImUyYjc5Nzc5LTBhODgtNDMzMS05YjQyLTM4NGNkNzFjODVkNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
