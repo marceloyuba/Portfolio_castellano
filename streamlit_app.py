6e4eaca3948dfe44e228f9dfeb7f2ed0d86404c8
marceloyuba <marceloyuba@gmail.com>
2024-03-17 19:36:07 -0300

    commit powerbi

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+from IPython.display import IFrame
 st.set_page_config(page_title="Prueba", page_icon=":tada:", layout="wide")
-
+dashboard = IFrame(src="https://app.powerbi.com/view?r=eyJrIjoiM2NjNDA0YmItMmRhZC00ZDhlLWFmOWYtZTZiMWMxYWY3ODAzIiwidCI6ImUyYjc5Nzc5LTBhODgtNDMzMS05YjQyLTM4NGNkNzFjODVkNyIsImMiOjR9", width=1000, height=600)
 # ---- HEADER SECTION ----
 with st.container():
     st.subheader("Hola, soy Marcelo Yuba :wave:")
@@ -10,3 +10,4 @@ with st.container():
         "Soy apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para negocios."
     )
     st.write("[Mas sobre mi >](https://github.com/marceloyuba)")
+with st.container(): dashboard
