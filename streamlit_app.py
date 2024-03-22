c780bdb46743f7addefbade589bc5f93a19292fa
marceloyuba <marceloyuba@gmail.com>
2024-03-22 14:11:54 -0300

    commit nueva

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import Functions
 import base64
 
 st.set_page_config(page_title="Porfolio Marcelo Yuba", page_icon="scr/fondo.jpg", layout="wide")
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
