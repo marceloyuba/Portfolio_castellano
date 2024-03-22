1234127b8e9004a145ea0de5e32e8b33539ebf7e
marceloyuba <marceloyuba@gmail.com>
2024-03-22 14:11:30 -0300

    commit nueva

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,29 +6,8 @@ st.set_page_config(page_title="Porfolio Marcelo Yuba", page_icon="scr/fondo.jpg"
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 local_css("style/style.css")
-
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-set_png_as_page_bg('scr/fondo.png')
 
 # ---- HEADER SECTION ----
 with st.container():
