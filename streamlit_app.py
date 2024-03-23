ba01673bf17e0f910557e68a52d8e9230ebfc634
marceloyuba <marceloyuba@gmail.com>
2024-03-23 05:51:54 -0300

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,14 +13,34 @@ with st.container():
     st.header("Hola, soy Marcelo Yuba :wave:")
     st.title("Un Data Analyst  y Data Scientist de Buenos Aires, Argentina")
     st.write(
-        "Soy un apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para tu negocio."
+        "Apasionado en el analisis de datos usando, Power BI y Python, tratando de forma mas eficiente,  obtener resultados para tu negocio."
     )
     st.write("[Mi Github >](https://github.com/marceloyuba)")
     st.write("[Mi LinkedIn >](https://www.linkedin.com/in/marcelo-yuba-b9a39827b/)")
 
 
 
+# ---- WHAT I DO ----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+        st.write(
+            """
+            On my YouTube channel I am creating tutorials for people who:
+            - are looking for a way to leverage the power of Python in their day-to-day work.
+            - are struggling with repetitive tasks in Excel and are looking for a way to use Python and VBA.
+            - want to learn Data Analysis & Data Science to perform meaningful and impactful analyses.
+            - are working with Excel and found themselves thinking - "there has to be a better way."
 
+            If this sounds interesting to you, consider subscribing and turning on the notifications, so you don’t miss any content.
+            """
+        )
+        st.write("[YouTube Channel >](https://youtube.com/c/CodingIsFun)")
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
 
 
 
