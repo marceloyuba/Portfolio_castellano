030ab4c4609605f3d7586af3f94f6c8f9fa33b70
marceloyuba <marceloyuba@gmail.com>
2024-03-23 06:28:46 -0300

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ with st.container():
     with col2:
         st.header("Imagen en la columna 2")
         imagen = "scr/taxis.png"  # Reemplaza esto con la ruta de tu imagen
-        st.image(imagen, caption='Descripción de la imagen', width=100)
+        st.image(imagen, width=100)
 
 
 
